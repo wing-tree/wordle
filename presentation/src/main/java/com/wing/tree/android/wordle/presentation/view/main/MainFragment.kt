e0c135bdb820d10bbaab71835a0e0e5eb46640ac
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-04-24 17:39:16 +0900

    feat : MainViewModel 추가 및 메인 화면 Statistics 보여주도록 처리

--- presentation/src/main/java/com/wing/tree/android/wordle/presentation/view/main/MainFragment.kt
+++ presentation/src/main/java/com/wing/tree/android/wordle/presentation/view/main/MainFragment.kt
@@ -2,17 +2,25 @@ package com.wing.tree.android.wordle.presentation.view.main
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.wing.tree.android.wordle.presentation.databinding.FragmentMainBinding
 import com.wing.tree.android.wordle.presentation.view.base.BaseFragment
+import com.wing.tree.android.wordle.presentation.viewmodel.main.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainFragment : BaseFragment<FragmentMainBinding>() {
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun inflate(inflater: LayoutInflater, container: ViewGroup?): FragmentMainBinding {
         return FragmentMainBinding.inflate(inflater, container, false)
     }
 
     override fun initData() {
-
+        viewModel.statistics.observe(viewLifecycleOwner) {
+            viewBinding.statisticsView.setStatistics(it)
+        }
     }
 
     override fun bind(viewBinding: FragmentMainBinding) {
---
+++ presentation/src/main/java/com/wing/tree/android/wordle/presentation/viewmodel/main/MainViewModel.kt
@@ -0,0 +1,26 @@
+package com.wing.tree.android.wordle.presentation.viewmodel.main
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.map
+import androidx.lifecycle.viewModelScope
+import com.wing.tree.android.wordle.domain.model.Statistics
+import com.wing.tree.android.wordle.domain.usecase.core.Result
+import com.wing.tree.android.wordle.domain.usecase.statistics.GetStatisticsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MainViewModel(
+    getStatisticsUseCase: GetStatisticsUseCase,
+    application: Application
+) : AndroidViewModel(application) {
+    val statistics = getStatisticsUseCase.invoke(Unit).asLiveData(viewModelScope.coroutineContext)
+        .map { result ->
+            when(result) {
+                is Result.Error -> Statistics.Default
+                is Result.Success -> result.data
+                Result.Loading -> Statistics.Default
+            }
+        }
+}
