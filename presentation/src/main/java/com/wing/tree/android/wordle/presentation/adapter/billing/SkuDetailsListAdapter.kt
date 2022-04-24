1b81a9a368c7910d05e5993663ba9ff105eae6dc
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-04-24 17:05:04 +0900

    feat : 구매 목록 어댑터 기본 작업

---
+++ presentation/src/main/java/com/wing/tree/android/wordle/presentation/adapter/billing/SkuDetailsListAdapter.kt
@@ -0,0 +1,54 @@
+package com.wing.tree.android.wordle.presentation.adapter.billing
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.android.billingclient.api.SkuDetails
+import com.wing.tree.android.wordle.presentation.databinding.SkuDetailsItemBinding
+import org.json.JSONException
+import org.json.JSONObject
+import timber.log.Timber
+
+class SkuDetailsListAdapter : ListAdapter<SkuDetails, SkuDetailsListAdapter.ViewHolder>(DiffCallback()) {
+    inner class ViewHolder(private val viewBinding: SkuDetailsItemBinding): RecyclerView.ViewHolder(viewBinding.root) {
+        fun bind(item: SkuDetails) {
+            with(viewBinding) {
+                textViewName.text = item.name
+                textViewPrice.text = item.price
+                textViewDescription.text = item.description
+            }
+        }
+
+        private val SkuDetails.jsonObject get() = JSONObject(originalJson)
+
+        private val SkuDetails.name: String get() = try {
+            jsonObject.getString("name")
+        } catch (e: JSONException) {
+            Timber.e(e)
+            title
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val viewBinding = SkuDetailsItemBinding.inflate(inflater, parent, false)
+
+        return ViewHolder(viewBinding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class DiffCallback: DiffUtil.ItemCallback<SkuDetails>() {
+        override fun areItemsTheSame(oldItem: SkuDetails, newItem: SkuDetails): Boolean {
+            return oldItem.sku == newItem.sku
+        }
+
+        override fun areContentsTheSame(oldItem: SkuDetails, newItem: SkuDetails): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
