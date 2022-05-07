91d85ff07c7de3957f1df74c0c8143cd36ff0517
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-05-07 23:01:09 +0900

    refactor : dependency clean up

--- presentation/src/main/java/com/wing/tree/android/wordle/presentation/widget/LetterView.kt
+++ presentation/src/main/java/com/wing/tree/android/wordle/presentation/widget/LetterView.kt
@@ -1,43 +1,23 @@
 package com.wing.tree.android.wordle.presentation.widget
 
 import android.content.Context
-import android.content.res.ColorStateList
 import android.util.AttributeSet
 import android.widget.FrameLayout
-import android.widget.TextView
-import androidx.annotation.ColorInt
 import androidx.core.view.isVisible
 import com.wing.tree.android.wordle.presentation.R
 import com.wing.tree.android.wordle.presentation.databinding.LetterViewBinding
-import com.wing.tree.android.wordle.presentation.extention.gone
-import com.wing.tree.android.wordle.presentation.extention.visible
 import com.wing.tree.android.wordle.presentation.model.play.Letter
 import com.wing.tree.android.wordle.presentation.util.flip
 
 class LetterView : FrameLayout, Flippable<LetterView> {
     private val viewBinding: LetterViewBinding = LetterViewBinding.bind(inflate(context, R.layout.letter_view, this))
+    private val isFlipped: Boolean get() = viewBinding.textViewBack.isVisible
 
     override var isFlippable = true
     override var isAnimating = false
 
-    private var back = viewBinding.letterBack
-
-    private var _front = viewBinding.letterFront
-    val front: TextView get() = _front
-
-    @ColorInt
-    var backBackgroundColor: Int = 0
-        set(value) {
-            field = value
-            back.backgroundTintList = ColorStateList.valueOf(field)
-        }
-
-    @ColorInt
-    var frontBackgroundColor: Int = 0
-        set(value) {
-            field = value
-            front.backgroundTintList = ColorStateList.valueOf(field)
-        }
+    private var back = viewBinding.textViewBack
+    private var front = viewBinding.textViewFront
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
@@ -48,27 +28,25 @@ class LetterView : FrameLayout, Flippable<LetterView> {
     )
 
     override fun flip(doOnEnd: ((LetterView) -> Unit)?) {
-        val letterView = this
+        if (isFlippable && isAnimating.not()) {
+            isAnimating = true
 
+            flip(back, front) {
+                swap()
+                doOnEnd?.invoke(this)
+                isAnimating = false
+            }
+        }
+    }
+
+    private fun swap() {
         with(viewBinding) {
-            if (isFlippable && isAnimating.not()) {
-                isAnimating = true
-
-                if (letterBack.isVisible) {
-                    flip(letterFront, letterBack) {
-                        back = letterBack
-                        _front = letterFront
-                        doOnEnd?.invoke(letterView)
-                        isAnimating = false
-                    }
-                } else {
-                    flip(letterBack, letterFront) {
-                        back = letterFront
-                        _front = letterBack
-                        doOnEnd?.invoke(letterView)
-                        isAnimating = false
-                    }
-                }
+            if (isFlipped) {
+                back = textViewFront
+                front = textViewBack
+            } else {
+                back = textViewBack
+                front = textViewFront
             }
         }
     }
