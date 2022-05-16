7afd0fd2f44f3829bf5f21b3f5ebe45cc9d0458d
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-05-17 06:55:36 +0900

    refactor : rename keyboard items in keyboard_view.xml and KeyboardView.kt

--- presentation/src/main/java/com/wing/tree/android/wordle/presentation/widget/KeyboardView.kt
+++ presentation/src/main/java/com/wing/tree/android/wordle/presentation/widget/KeyboardView.kt
@@ -45,34 +45,34 @@ class KeyboardView : LinearLayout {
     private fun bind() {
         val onClickListener = OnClickListener { view ->
             val key = when(view.id) {
-                R.id.key_a -> Key.Alphabet("a")
-                R.id.key_b -> Key.Alphabet("b")
-                R.id.key_c -> Key.Alphabet("c")
-                R.id.key_d -> Key.Alphabet("d")
-                R.id.key_e -> Key.Alphabet("e")
-                R.id.key_f -> Key.Alphabet("f")
-                R.id.key_g -> Key.Alphabet("g")
-                R.id.key_h -> Key.Alphabet("h")
-                R.id.key_i -> Key.Alphabet("i")
-                R.id.key_j -> Key.Alphabet("j")
-                R.id.key_k -> Key.Alphabet("k")
-                R.id.key_l -> Key.Alphabet("l")
-                R.id.key_m -> Key.Alphabet("m")
-                R.id.key_n -> Key.Alphabet("n")
-                R.id.key_o -> Key.Alphabet("o")
-                R.id.key_p -> Key.Alphabet("p")
-                R.id.key_q -> Key.Alphabet("q")
-                R.id.key_r -> Key.Alphabet("r")
-                R.id.key_s -> Key.Alphabet("s")
-                R.id.key_t -> Key.Alphabet("t")
-                R.id.key_u -> Key.Alphabet("u")
-                R.id.key_v -> Key.Alphabet("v")
-                R.id.key_w -> Key.Alphabet("w")
-                R.id.key_x -> Key.Alphabet("x")
-                R.id.key_y -> Key.Alphabet("y")
-                R.id.key_z -> Key.Alphabet("z")
-                R.id.key_return -> Key.Return
-                R.id.key_backspace -> Key.Backspace
+                R.id.image_view_backspace -> Key.Backspace
+                R.id.image_view_return -> Key.Return
+                R.id.key_view_a -> Key.Alphabet("a")
+                R.id.key_view_b -> Key.Alphabet("b")
+                R.id.key_view_c -> Key.Alphabet("c")
+                R.id.key_view_d -> Key.Alphabet("d")
+                R.id.key_view_e -> Key.Alphabet("e")
+                R.id.key_view_f -> Key.Alphabet("f")
+                R.id.key_view_g -> Key.Alphabet("g")
+                R.id.key_view_h -> Key.Alphabet("h")
+                R.id.key_view_i -> Key.Alphabet("i")
+                R.id.key_view_j -> Key.Alphabet("j")
+                R.id.key_view_k -> Key.Alphabet("k")
+                R.id.key_view_l -> Key.Alphabet("l")
+                R.id.key_view_m -> Key.Alphabet("m")
+                R.id.key_view_n -> Key.Alphabet("n")
+                R.id.key_view_o -> Key.Alphabet("o")
+                R.id.key_view_p -> Key.Alphabet("p")
+                R.id.key_view_q -> Key.Alphabet("q")
+                R.id.key_view_r -> Key.Alphabet("r")
+                R.id.key_view_s -> Key.Alphabet("s")
+                R.id.key_view_t -> Key.Alphabet("t")
+                R.id.key_view_u -> Key.Alphabet("u")
+                R.id.key_view_v -> Key.Alphabet("v")
+                R.id.key_view_w -> Key.Alphabet("w")
+                R.id.key_view_x -> Key.Alphabet("x")
+                R.id.key_view_y -> Key.Alphabet("y")
+                R.id.key_view_z -> Key.Alphabet("z")
                 else -> throw IllegalArgumentException("${view.id}")
             }
 
@@ -81,66 +81,66 @@ class KeyboardView : LinearLayout {
         }
 
         with(viewBinding) {
-            keyA.setOnClickListener(onClickListener)
-            keyB.setOnClickListener(onClickListener)
-            keyC.setOnClickListener(onClickListener)
-            keyD.setOnClickListener(onClickListener)
-            keyE.setOnClickListener(onClickListener)
-            keyF.setOnClickListener(onClickListener)
-            keyG.setOnClickListener(onClickListener)
-            keyH.setOnClickListener(onClickListener)
-            keyI.setOnClickListener(onClickListener)
-            keyJ.setOnClickListener(onClickListener)
-            keyK.setOnClickListener(onClickListener)
-            keyL.setOnClickListener(onClickListener)
-            keyM.setOnClickListener(onClickListener)
-            keyN.setOnClickListener(onClickListener)
-            keyO.setOnClickListener(onClickListener)
-            keyP.setOnClickListener(onClickListener)
-            keyQ.setOnClickListener(onClickListener)
-            keyR.setOnClickListener(onClickListener)
-            keyS.setOnClickListener(onClickListener)
-            keyT.setOnClickListener(onClickListener)
-            keyU.setOnClickListener(onClickListener)
-            keyV.setOnClickListener(onClickListener)
-            keyW.setOnClickListener(onClickListener)
-            keyX.setOnClickListener(onClickListener)
-            keyY.setOnClickListener(onClickListener)
-            keyZ.setOnClickListener(onClickListener)
-            keyReturn.setOnClickListener(onClickListener)
-            keyBackspace.setOnClickListener(onClickListener)
+            imageViewBackspace.setOnClickListener(onClickListener)
+            imageViewReturn.setOnClickListener(onClickListener)
+            keyViewA.setOnClickListener(onClickListener)
+            keyViewB.setOnClickListener(onClickListener)
+            keyViewC.setOnClickListener(onClickListener)
+            keyViewD.setOnClickListener(onClickListener)
+            keyViewE.setOnClickListener(onClickListener)
+            keyViewF.setOnClickListener(onClickListener)
+            keyViewG.setOnClickListener(onClickListener)
+            keyViewH.setOnClickListener(onClickListener)
+            keyViewI.setOnClickListener(onClickListener)
+            keyViewJ.setOnClickListener(onClickListener)
+            keyViewK.setOnClickListener(onClickListener)
+            keyViewL.setOnClickListener(onClickListener)
+            keyViewM.setOnClickListener(onClickListener)
+            keyViewN.setOnClickListener(onClickListener)
+            keyViewO.setOnClickListener(onClickListener)
+            keyViewP.setOnClickListener(onClickListener)
+            keyViewQ.setOnClickListener(onClickListener)
+            keyViewR.setOnClickListener(onClickListener)
+            keyViewS.setOnClickListener(onClickListener)
+            keyViewT.setOnClickListener(onClickListener)
+            keyViewU.setOnClickListener(onClickListener)
+            keyViewV.setOnClickListener(onClickListener)
+            keyViewW.setOnClickListener(onClickListener)
+            keyViewX.setOnClickListener(onClickListener)
+            keyViewY.setOnClickListener(onClickListener)
+            keyViewZ.setOnClickListener(onClickListener)
         }
     }
 
     private fun findViewByKey(key: String): KeyView? {
         return with(viewBinding) {
             when (key.lowercase()) {
-                "a" -> keyA
-                "b" -> keyB
-                "c" -> keyC
-                "d" -> keyD
-                "e" -> keyE
-                "f" -> keyF
-                "g" -> keyG
-                "h" -> keyH
-                "i" -> keyI
-                "j" -> keyJ
-                "k" -> keyK
-                "l" -> keyL
-                "m" -> keyM
-                "n" -> keyN
-                "o" -> keyO
-                "p" -> keyP
-                "q" -> keyQ
-                "r" -> keyR
-                "s" -> keyS
-                "t" -> keyT
-                "u" -> keyU
-                "v" -> keyV
-                "w" -> keyW
-                "x" -> keyX
-                "y" -> keyY
-                "z" -> keyZ
+                "a" -> keyViewA
+                "b" -> keyViewB
+                "c" -> keyViewC
+                "d" -> keyViewD
+                "e" -> keyViewE
+                "f" -> keyViewF
+                "g" -> keyViewG
+                "h" -> keyViewH
+                "i" -> keyViewI
+                "j" -> keyViewJ
+                "k" -> keyViewK
+                "l" -> keyViewL
+                "m" -> keyViewM
+                "n" -> keyViewN
+                "o" -> keyViewO
+                "p" -> keyViewP
+                "q" -> keyViewQ
+                "r" -> keyViewR
+                "s" -> keyViewS
+                "t" -> keyViewT
+                "u" -> keyViewU
+                "v" -> keyViewV
+                "w" -> keyViewW
+                "x" -> keyViewX
+                "y" -> keyViewY
+                "z" -> keyViewZ
                 else -> null
             }
         }
