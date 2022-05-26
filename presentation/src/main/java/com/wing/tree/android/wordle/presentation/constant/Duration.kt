d20bfc8bfb9f618b853cf60a04ffc5eefaf8ea2f
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-05-26 20:25:36 +0900

    feat : apply item consume button design

--- presentation/src/main/java/com/wing/tree/android/wordle/presentation/constant/Duration.kt
+++ presentation/src/main/java/com/wing/tree/android/wordle/presentation/constant/Duration.kt
@@ -2,8 +2,8 @@ package com.wing.tree.android.wordle.presentation.constant
 
 object Duration {
     object Animation {
-        const val FADE_IN = 120L
-        const val FADE_OUT = 120L
+        const val FADE_IN = 240L
+        const val FADE_OUT = 240L
         const val SCALE_UP_DOWN = 120L
         const val TRANSITION = 240
     }
--- presentation/src/main/java/com/wing/tree/android/wordle/presentation/widget/ItemConsumptionFloatingActionButton.kt
+++ presentation/src/main/java/com/wing/tree/android/wordle/presentation/widget/ItemConsumptionFloatingActionButton.kt
@@ -5,6 +5,8 @@ import android.util.AttributeSet
 import android.widget.FrameLayout
 import com.wing.tree.android.wordle.presentation.R
 import com.wing.tree.android.wordle.presentation.databinding.ItemConsumptionFloatingActionButtonBinding
+import com.wing.tree.android.wordle.presentation.extention.fadeIn
+import com.wing.tree.android.wordle.presentation.extention.fadeOut
 import com.wing.tree.android.wordle.presentation.extention.gone
 import com.wing.tree.android.wordle.presentation.extention.visible
 import com.wing.tree.wordle.core.util.isZero
@@ -18,12 +20,12 @@ class ItemConsumptionFloatingActionButton : FrameLayout {
 
             with(viewBinding) {
                 if (field.isZero) {
-                    textViewCount.gone()
-                    linearLayoutCredits.visible()
+                    linearLayoutCredits.fadeIn()
+                    rollingTextViewCount.fadeOut()
                 } else {
-                    linearLayoutCredits.gone()
-                    textViewCount.visible()
-                    textViewCount.text = "$field"
+                    linearLayoutCredits.fadeOut()
+                    rollingTextViewCount.fadeIn()
+                    rollingTextViewCount.setText("$field")
                 }
             }
         }
@@ -46,12 +48,13 @@ class ItemConsumptionFloatingActionButton : FrameLayout {
 
         val src = typedArray.getDrawable(R.styleable.ItemConsumptionFloatingActionButton_src)
         val tint = typedArray.getColorStateList(R.styleable.ItemConsumptionFloatingActionButton_tint)
-        val tintDark = typedArray.getColor(R.styleable.ItemConsumptionFloatingActionButton_tint_dark, 0)
+        val tintDark = typedArray.getColorStateList(R.styleable.ItemConsumptionFloatingActionButton_tint_dark)
 
         with(viewBinding) {
             floatingActionButton.backgroundTintList = tint
             floatingActionButton.setImageDrawable(src)
-            linearLayoutCredits.setBackgroundColor(tintDark)
+            linearLayoutCredits.backgroundTintList = tintDark
+            rollingTextViewCount.animationDuration = 240L
         }
         typedArray.recycle()
     }
