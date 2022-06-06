a8d3a82bafc9c517e7c5a15fc80726d4d959d7be
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-06-06 20:56:37 +0900

    feat : add 'int' extension property and apply 'unused' annotation on Util.kt

--- core/src/main/java/com/wing/tree/wordle/core/util/Util.kt
+++ core/src/main/java/com/wing/tree/wordle/core/util/Util.kt
@@ -1,5 +1,8 @@
+@file:Suppress("unused")
+
 package com.wing.tree.wordle.core.util
 
 val Int.half: Int get() = this / 2
 val Int.isZero: Boolean get() = this == 0
 val Int.isNotZero: Boolean get() = this != 0
+val Float.int: Int get() = toInt()
