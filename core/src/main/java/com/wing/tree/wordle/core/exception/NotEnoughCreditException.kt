5e7d239098d3c7d8dad388cc32f40ccf67d274de
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-06-11 10:37:11 +0900

    refactor : gradle version up

--- core/src/main/java/com/wing/tree/wordle/core/exception/NotEnoughCreditException.kt
+++ core/src/main/java/com/wing/tree/wordle/core/exception/NotEnoughCreditException.kt
@@ -1,8 +1,11 @@
+@file:Suppress("unused")
+
 package com.wing.tree.wordle.core.exception
 
 class NotEnoughCreditException: Exception {
     constructor(): super()
     constructor(message: String): super(message)
+    @Suppress("UNUSED_PARAMETER")
     constructor(message: String, cause: Throwable): super(message)
     constructor(cause: Throwable): super(cause)
 }
